73a33eef0baea26f3af94d69790a5cfb71c02a16
SergioSilva <sergiobam159@gmail.com>
2020-10-23 14:29:53 -0500

    implementada funcion anti injection

--- noticias/Controllers/UsuarioController.cs
+++ noticias/Controllers/UsuarioController.cs
@@ -22,21 +22,46 @@ namespace noticias.Controllers
 
         }
        
+        public bool verificarTexto(string texto)
+        {
+            var validarExpresionRegular = new System.Text.RegularExpressions.Regex(@"^[a-zA-Z0-9]*$");
+
+            if (!validarExpresionRegular.IsMatch(texto))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
         [HttpPost]
         public ActionResult Verificar(Usuario acc)
         {
-            
-            con = conexion.Instancia.Conectar();
-            con.Open();
-            com.Connection = con;
-            com.CommandText = "select * from Usuario where cNombreUsuario='"+acc.cNombreUsuario+"' and cClaveUsuario='"+acc.cClaveUsuario+"'";
-            dr = com.ExecuteReader();
-            
-            if (dr.Read())
-            {
+            bool validarNombre = verificarTexto(acc.cNombreUsuario);
+            bool validarContraseña = verificarTexto(acc.cClaveUsuario);
 
-                return RedirectToAction("vistaCrud", "Noticia", new {inicial=0, elementos = 1 });
+            if (validarNombre && validarContraseña)
+            {
+                con = conexion.Instancia.Conectar();
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "select * from Usuario where cNombreUsuario='" + acc.cNombreUsuario + "' and cClaveUsuario='" + acc.cClaveUsuario + "'";
+                dr = com.ExecuteReader();
+
+                if (dr.Read())
+                {
+
+                    return RedirectToAction("vistaCrud", "Noticia", new { inicial = 0, elementos = 1 });
+                }
+                else
+                {
+                    con.Close();
+                    return View("Error");
+                }
             }
+            
             else
             {
                 con.Close();

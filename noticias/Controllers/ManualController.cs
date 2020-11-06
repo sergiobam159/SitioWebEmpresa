d0bd6cf602a81455aaf7c324f8c11d24c5393756
SergioSilva <sergiobam159@gmail.com>
2020-11-05 22:08:00 -0500

    CREACION DE OBJETO MANUAL PENDIENTE MODIFICAR LOGIN

---
+++ noticias/Controllers/ManualController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace noticias.Controllers
+{
+    public class ManualController : Controller
+    {
+        // GET: Manual
+        public ActionResult ControlesCrud()
+        {
+            return View();
+        }
+    }
+}
---
+++ noticias/Models/Manual.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace noticias.Models
+{
+    public class Manual
+    {
+        public int nIdManual;
+        public string cNombreManual;
+        public string cDescripcion;
+        public Boolean bEstado;
+        public string cPadre;
+        public string cJerarquia;
+        public string cTipoDocumento;
+        public int cUsuCodigo;
+        public string version;
+        public DateTime dFechaRegistro;
+        public string ruta;
+
+        public int NIdManual { get => nIdManual; set => nIdManual = value; }
+        public string CNombreManual { get => cNombreManual; set => cNombreManual = value; }
+        public string CDescripcion { get => cDescripcion; set => cDescripcion = value; }
+        public bool BEstado { get => bEstado; set => bEstado = value; }
+        public string CPadre { get => cPadre; set => cPadre = value; }
+        public string CJerarquia { get => cJerarquia; set => cJerarquia = value; }
+        public string CTipoDocumento { get => cTipoDocumento; set => cTipoDocumento = value; }
+        public int CUsuCodigo { get => cUsuCodigo; set => cUsuCodigo = value; }
+        public string Version { get => version; set => version = value; }
+        public DateTime DFechaRegistro { get => dFechaRegistro; set => dFechaRegistro = value; }
+        public string Ruta { get => ruta; set => ruta = value; }
+    }
+}
